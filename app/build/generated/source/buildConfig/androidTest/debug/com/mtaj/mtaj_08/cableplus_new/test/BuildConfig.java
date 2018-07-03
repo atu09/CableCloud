5090d2852687034211401b73051bd8a83b8a9620
Atirek Pothiwala <atirekpothiwala@gmail.com>
2018-07-04 00:56:47 +0530

    Fragment Issue Solved Customer Area List UI changed Customer List UI Changed Customer Master Details UI changed Contact Details UI Changed Make Payment UI Changed Few Icons Updated

---
+++ app/build/generated/source/buildConfig/androidTest/debug/com/mtaj/mtaj_08/cableplus_new/test/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.mtaj.mtaj_08.cableplus_new.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.mtaj.mtaj_08.cableplus_new.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 9;
+  public static final String VERSION_NAME = "3.0";
+}
